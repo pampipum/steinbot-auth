34513ae651c5bb890a6e0761193d89afcffea1ae
Andre Lopes <andremarqueslps@hotmail.comm>
2024-02-22 15:14:45 +0530

    Bunch of new stuff

--- src/routes/stripe/checkout/+page.server.ts
+++ src/routes/stripe/checkout/+page.server.ts
@@ -1,44 +1,43 @@
-import type { Actions } from "./$types";
-import { error, redirect, type Redirect } from "@sveltejs/kit";
-import Stripe from "stripe";
-import { VITE_STRIPE_SECRET_KEY } from "$env/static/private";
+import type { Actions } from './$types';
+import { error, redirect, type Redirect } from '@sveltejs/kit';
+import Stripe from 'stripe';
+import { VITE_STRIPE_SECRET_KEY } from '$env/static/private';
 
-
-const stripe = new Stripe(VITE_STRIPE_SECRET_KEY, { apiVersion: "2022-11-15" });
+const stripe = new Stripe(VITE_STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' });
 
 export const actions: Actions = {
-  default: async ({ request}) => {
-    let url: string | null;
+	default: async ({ request }) => {
+		let url: string | null;
 
-    try {
-      // Extract the form data from the request
-      const formData = await request.formData();
-      const userId = formData.get('userId')?.toString(); // Ensure userId is a string
-      console.log("Before creating session");
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: "price_1OhpmOD73Fyj3iUEvPMio1xd",
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
-        success_url: `${request.headers.get("origin")}/?success=true`,
-        cancel_url: `${request.headers.get("origin")}/?cancelled=true`,
-        metadata: {
-          userId: userId, // Add the userId to the metadata
-        }
-      });
-      console.log("After creating session");
+		try {
+			// Extract the form data from the request
+			const formData = await request.formData();
+			const userId = formData.get('userId')?.toString(); // Ensure userId is a string
+			console.log('Before creating session');
+			const session = await stripe.checkout.sessions.create({
+				line_items: [
+					{
+						price: 'price_1OmYoyD73Fyj3iUEUQ5oFIlO',
+						quantity: 1
+					}
+				],
+				mode: 'payment',
+				success_url: `${request.headers.get('origin')}/?success=true`,
+				cancel_url: `${request.headers.get('origin')}/?cancelled=true`,
+				metadata: {
+					userId: userId // Add the userId to the metadata
+				}
+			});
+			console.log('After creating session');
 
-      url = session.url;
-    } catch (errorObj) {
-      throw error(500, "Unknown error occurred");
-    }
+			url = session.url;
+		} catch (errorObj) {
+			throw error(500, 'Unknown error occurred');
+		}
 
-    if (url) {
-      console.log(url);
-      throw redirect(303, url);
-    }
-  },
+		if (url) {
+			console.log(url);
+			throw redirect(303, url);
+		}
+	}
 };
